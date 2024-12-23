d3b8f13140aa4f08ba1dad1b11cb4ade4c5cbcd1
Moonyflay <61058829+Moonyflay@users.noreply.github.com>
2024-12-23 17:27:42 +0300

    Update eval_utils.py
    Added history plotting
    Added GridSearch parameter save
    Added TPR, FPR save

--- eval_utils.py
+++ eval_utils.py
@@ -2,6 +2,7 @@ import yaml
 import json
 import os
 from datetime import datetime
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import precision_recall_curve, roc_curve
 
@@ -38,11 +39,24 @@ def plot_and_save_curves(y_test, y_pred_proba, log_dir):
     plt.savefig(roc_curve_path)
     plt.close()
 
-def save_logs(config, metrics, dataset_name, y_test, y_pred_proba, log_dir=None):
+    # Save fpr and tpr
+    rates = np.empty((len(fpr), 2))
+    rates[:, 0] = fpr
+    rates[:, 1] = tpr
+    rates = rates.tolist()
+    rates_path = os.path.join(log_dir, "rates.json")
+    with open(rates_path, 'w') as file:
+        json.dump(rates, file, indent=4)
+
+
+
+def save_logs(config, metrics, dataset_name, y_test, y_pred_proba, log_dir=None, gs_params=None, history=None):
     if log_dir is None:
         # Create log directory
         time_str = datetime.now().strftime("%Y.%m.%d_%H.%M.%S")
         log_dir = os.path.join("logs", dataset_name, time_str)
+        if gs_params is not None:
+            log_dir = os.path.join("gridsearch", dataset_name, time_str)
         os.makedirs(log_dir, exist_ok=True)
 
     # Save config
@@ -58,4 +72,36 @@ def save_logs(config, metrics, dataset_name, y_test, y_pred_proba, log_dir=None)
         # Save plots
         plot_and_save_curves(y_test, y_pred_proba, log_dir)
 
+    # Save gridsearch parameters
+    if gs_params is not None:
+        gs_params_path = os.path.join(log_dir, "gs_params.json")
+        with open(gs_params_path, 'w') as file:
+            json.dump(gs_params, file, indent=4)
+    
+    # Save convergence plot
+    if history is not None:
+        plot_history(history, log_dir)
+    
     return log_dir
+
+    
+def plot_history(history, log_dir):
+    epoch_num = len(history)
+    names_list = history[0].keys()
+    metrics_arr = [[metrics[name] for metrics in history] for name in names_list]
+    metrics_arr = np.array(metrics_arr)
+    plt.figure()
+    for i, name in enumerate(names_list):
+        if (name == 'precision') or (name == 'recall'):
+            continue
+        plt.plot(np.arange(1, 1+epoch_num), metrics_arr[i, :], label=name)
+    plt.ylim((0.5, 1))
+    plt.xlabel('Epoch')
+    plt.ylabel('Metric value')
+    plt.title('Convergence plot')
+    plt.legend()
+    plt.grid(True)
+    plot_path = os.path.join(log_dir, 'convergence.png')
+    plt.savefig(plot_path)
+    plt.close()
+    return
